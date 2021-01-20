e9bdbdfa0bddbdd7de19dc458ca8040a04bb83a0
cabanes <cabanes@login2.occigen>
2021-01-20 14:49:47 +0100

    add FFT and windowing

---
+++ preprocess/FourierTransform1D.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+#
+def fft1D ( Y, N ):
+
+	import numpy as np
+	import math
+	import scipy.special as sp
+	#import SH
+	#import Hanning
+
+#FourierTransform un signal Y de taille N pour lequel on fait une
+#transformee de fourier discrete.
+# FFT classic
+# fft(1)=Reel est la cste +1 du signal y=Amp*sin(x) +1; 
+# fft(2:N/2)=imag de la transformee de fourier.
+# fft(N/2+1)=Reel est la frq de coupure de lunquist pour un echantillonage N.
+# INPUT: le signal Y et sa taille N
+# OUTPUT: le signal FFT est de taille N/2+1
+# si not un integer alors les indices pour stocker la matrice sont non entier plus bas
+	if float(N/2.).is_integer() == False:
+		N=N-1
+#--------------------------------------------------------------------------
+#                             NOTES SUR PSD & FFT
+#--------------------------------------------------------------------------
+# Note: from ==> http://www.gaussianwaves.com/2013/12/computation-of-power-of-a-signal-in-matlab-simulation-and-verification/
+# Note se rapportant a l image EnergySpectrum.jpg au lien C:\Users\simon\Desktop\Num_Jupiter\Nek5000\Routines
+# Une fft doit etre normalisee par N dans matlab, ceci n est pas fait
+# automatiquement avec la routine fft de matlab. En revanche la ifft
+# normalise par N. Conclusion abs(FFT_Y/N) donne une amplitude des modes
+# Amp/2 (voir image EnergySpectrum.jpg). Donc je peux multiplier par 2 voir**.
+# De meme pour la power spectral density PSD = (1/(N*N)) * abs(xdft).^2 =
+# Amp^2/4. Donc je peux multiplier par 4 pour connaitre l amplitude au
+# carre voir***: LA BELLE AFFAIRE!!!
+#--------------------------------------------------------------------------
+#                             NOTES SUR FFT
+#--------------------------------------------------------------------------
+# Dans FT_Y et FT_Y2 = abs(FT_Y/N) on a: 1/ FT_Y(1) est reel et est la constante du
+# signal (non nulle si le signal est de moyenne non nulle) soit le mode
+# m=0. 2/ FT_Y(2:N/2) contient tous les modes aux nombre de N/2-1 (m=0 etant a part). N/2 etant la frequence
+# max de lunquist contraint par la taille initiale de l echantillonnage
+# dans le signal. 3/ FT_Y(N/2+1) est un reel egalement corrspondant a la
+# frequence de Lunquist. 4/ FT_Y(N/2+2:end) on retrouve les N/2 modes qui
+# sont les conjugues de FT_Y(2:N/2). On a un signal FT_Y de taille L = 2 *
+# (N/2-1) + 2.
+# -----Tranformer de fourier par FFT
+	FFT_Y = np.fft.fft(Y) # D
+	FFT_Y2 = abs(FFT_Y/N)
+	FFT_Y1 = FFT_Y2[0:N/2+1] # 
+	FFT_Y1[1:-2] = 2*FFT_Y1[1:-2] # asterix **
+
+# -----energy spectra
+	xdft = np.fft.fft(Y)
+	xdft = xdft[0:N/2+1]
+	psdx = (1./(N*N)) * abs(xdft)**2
+	psdx[1:-2] = 4.*psdx[1:-2]  # Asterix *** , autre exemple: ==>  http://fr.mathworks.com/help/signal/ug/psd-estimate-using-fft.html
+	E_Y = psdx
+	m = np.arange(0.,N/2+1) # Attention!!! la gamme des modes demarre a 0 et pas 1, lunquist inclu
+	return( FFT_Y1,E_Y,m )
---
+++ preprocess/windowing.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+#
+def WindowingHanning ( Y,N,L,iP ):
+# INPUT: N la taille du signal, Y le signal de N echantillon en temps ou espace.
+#	 L longueur du signal dans  l unite souhaitee.
+# OUTPUT: Y_Han de taille N 
+
+# % % Y*H(n) % on va perdre ici en amplitude du signal, si AMP=1 dans H
+# ==> l'apmplitude est comprise entre 0 (au bord) et 1.
+# avec Hanning on va diminuer l amplitude du signal par 2, integral de la
+# courbe une fois passee dans la fenetre de hanning en revanche le pic en
+# frq est plus marque, le signal etant periodisise.
+# ==> En revanche les basses frquencs sont plus energetiques (hanning ajoute
+# artificiellement un signal basse frequence) mais les hautes frqce sont
+# mieux resolues.
+# ==> si le signal est deja periodique on gagne rien avec hanning on perd
+# au contraire les resolution basse frq en revanche si non periodique on
+# perd bcp dans une fft classique
+	import numpy as np
+	import math
+	import matplotlib.pyplot as plt
+	import matplotlib.patches as mpatches
+
+# ==> hanning function
+	H=0.5*(1.-np.cos(2.*np.pi*np.arange(0.,N,1.)/(N-1.)))
+	r=np.arange(0.,N,1.)/(N-1.)
+# ==> windowed signal
+	Y_Han = Y*np.transpose(H);
+	if(iP==1):
+		fig = plt.figure()	
+		plt.plot(r*L,Y_Han, color='black')
+		plt.plot(r*L,Y, color='darkred')
+		plt.xlabel('space/time variable')
+		plt.ylabel('Y_windowed')
+		plt.grid()
+		plt.show(fig)
+	return (Y_Han)
+
+
+def WindowingTukey ( Y,N,L,iP ):
+# INPUT: N la taille du signal, Y le signal de N echantillon en temps ou espace.
+#	 L longueur du signal dans  l unite souhaitee.
+# OUTPUT: Y_Han de taille N 
+
+# % % Y*H(n) % on va perdre ici en amplitude du signal, si AMP=1 dans H
+# ==> l'apmplitude est comprise entre 0 (au bord) et 1.
+# avec Hanning on va diminuer l amplitude du signal par 2, integral de la
+# courbe une fois passee dans la fenetre de hanning en revanche le pic en
+# frq est plus marque, le signal etant periodisise.
+# ==> En revanche les basses frquencs sont plus energetiques (hanning ajoute
+# artificiellement un signal basse frequence) mais les hautes frqce sont
+# mieux resolues.
+# ==> si le signal est deja periodique on gagne rien avec hanning on perd
+# au contraire les resolution basse frq en revanche si non periodique on
+# perd bcp dans une fft classique
+	import numpy as np
+	import math
+	import matplotlib.pyplot as plt
+	import matplotlib.patches as mpatches
+
+# ==> Tukey function
+	T = np.zeros([N])
+	alpha = 0.5
+	n = np.arange(0.,N,1.)
+	n1 = alpha*(N-1)/2.
+	n2 = (N-1)*(1-alpha/2.)
+	for iN in range(0, len(n)):
+		if(n[iN] < n1):
+			T[iN] = 0.5*(1.+np.cos(np.pi*(2*n[iN]/(alpha*(N-1.)) - 1.)))
+		elif(n[iN] <= n2):
+			T[iN] = 1.
+		else:
+			T[iN] = 0.5*(1.+np.cos(np.pi*(2*n[iN]/(alpha*(N-1.)) - 2./alpha + 1.)))
+	r=np.arange(0.,N,1.)/(N-1.)
+# ==> windowed signal
+	Y_Tukey = Y*np.transpose(T)
+	if(iP==1):
+		fig = plt.figure()	
+		plt.plot(r*L,Y_Tukey, color='black')
+		plt.plot(r*L,Y, color='darkred')
+		plt.xlabel('space/time variable')
+		plt.ylabel('Y_windowed')
+		plt.grid()
+		plt.show(fig)
+	return (Y_Tukey)
